198f72827968e0e28fc575e3b08ed28b7fc42cd0
root <iratao@gmail.com>
2024-10-01 08:57:36 +0800

    code polish for day2

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -2,20 +2,15 @@
  * Try to use PromtTemplates to format promts.
  */
 import { ChatOpenAI } from "@langchain/openai";
-import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 import { StringOutputParser } from "@langchain/core/output_parsers";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
-
 
 import '../bootstrap';
 
 const model = new ChatOpenAI({ model: "gpt-4" });
 const parser = new StringOutputParser();
 
-
-
 const systemTemplate = "Given an example of how the text is used in a sentence: {context}, translate the following text into {language}. ";
-
 
 const promptTemplate = ChatPromptTemplate.fromMessages([
   ["system", systemTemplate],
